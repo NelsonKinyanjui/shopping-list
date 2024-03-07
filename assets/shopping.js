28e76c42c40de25d2f992df3aae04590f74fd000
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-03-07 11:39:08 +0300

    create Child variables

--- assets/shopping.js
+++ assets/shopping.js
@@ -5,7 +5,19 @@ const inputBox = document.querySelector('input');
 
 const  addButton = document.querySelector('button');
 
+
 function addProduct(){
     let product;
 product.textContent = '';
 }
+
+addButton.addEventListener('click',addProduct);
+
+const listItem= document.createElement('li');
+
+const span = document.createElement('span');
+
+const delButton = document.createElement('button');
+
+list.appendChild();
+list.appendChild();
