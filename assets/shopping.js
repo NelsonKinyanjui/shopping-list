a660f161c814cd31c24839f9204d995724e66841
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-03-07 11:23:43 +0300

    create main variables

---
+++ assets/shopping.js
@@ -0,0 +1,6 @@
+// create variables needed first
+const  list = document.querySelector('ul');
+
+const inputBox = document.querySelector('input');
+
+const  addButton = document.querySelector('button');
